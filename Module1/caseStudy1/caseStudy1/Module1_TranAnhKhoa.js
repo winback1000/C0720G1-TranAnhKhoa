820bbe063edba31a0e85ba653604ea9646067210
winback1000 <68835331+winback1000@users.noreply.github.com>
2020-08-16 01:34:48 +0700

    update case study

--- Module1/caseStudy1/caseStudy1/Module1_TranAnhKhoa.js
+++ Module1/caseStudy1/caseStudy1/Module1_TranAnhKhoa.js
@@ -1,17 +1,40 @@
 function getinfo() {
-    let namevar = document.getElementById("name").value;
+    var namevar = document.getElementById("name").value;
     console.log(namevar);
-    let identityNumbervar = document.getElementById("identityNumber").value;
-    let dateOfBirthvar = document.getElementById("dateOfBirth").value;
-    let emailvar = document.getElementById("email").value;
-    let addressvar = document.getElementById("address").value;
-    let vipTypevar = document.getElementById("vipType").value;
-    let discountvar = parseFloat(document.getElementById("discount").value);
-    let peoplevar = document.getElementById("people").value;
-    let rentDayvar = parseInt(document.getElementById("rentDay").value);
-    let serviceTypevar = parseFloat(document.getElementById("serviceType").value);
-    let roomTypevar = document.getElementById("roomType").value;
-    let cost = serviceTypevar * rentDayvar * (1-(discountvar/100));
-    alert(cost+"$");
-    document.getElementById("estimatedcost").value=cost;
-    }
+    var identityNumbervar = document.getElementById("identityNumber").value;
+    var dateOfBirthvar = document.getElementById("dateOfBirth").value;
+    var emailvar = document.getElementById("email").value;
+    var addressvar = document.getElementById("address").value;
+    var vipTypevar = document.getElementById("vipType").value;
+    var discountvar = parseFloat(document.getElementById("discount").value);
+    var peoplevar = document.getElementById("people").value;
+    var rentDayvar = parseInt(document.getElementById("rentDay").value);
+    var serviceTypevar = parseFloat(document.getElementById("serviceType").value);
+    var roomTypevar = document.getElementById("roomType").value;
+    var cost = serviceTypevar * rentDayvar * (1-(discountvar/100));
+    let elem = document.getElementById('div1');
+    elem.style.display= 'none';
+    let elem2 = document.getElementById('div2');
+    elem2.style.display='block';
+    document.getElementById('greeting').innerHTML = ('Greeting '+namevar+' !<br><br>Welcome to the XXX Resort!')
+    document.getElementById('profileName').innerHTML = (namevar);
+    document.getElementById('profileIdentityNumber').innerHTML = (identityNumbervar);
+    document.getElementById('profileDateOfBirth').innerHTML = (dateOfBirthvar);
+    document.getElementById('profileEmail').innerHTML = (emailvar);
+    document.getElementById('profileAddress').innerHTML = (addressvar);
+    document.getElementById('profileVipType').innerHTML = (vipTypevar);
+    document.getElementById('profileDiscount').innerHTML = (discountvar);
+    document.getElementById('profileAmountPeople').innerHTML = (peoplevar);
+    document.getElementById('profileRentDay').innerHTML = (rentDayvar);
+    document.getElementById('profileServiceType').innerHTML = (serviceTypevar);
+    document.getElementById('profileRoomType').innerHTML = (roomTypevar);
+
+    document.getElementById('ProfileEstimatePrice').innerHTML = (cost+' $');
+}
+
+function edit() {
+    let elem2 = document.getElementById('div2');
+    elem2.style.display='none';
+    let elem = document.getElementById('div1');
+    elem.style.display= 'block';
+}
