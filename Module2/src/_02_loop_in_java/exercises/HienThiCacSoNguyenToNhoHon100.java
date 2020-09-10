330645a6b84c7380b1efa6b7d6a6278674cc1f4d
winback1000 <68835331+winback1000@users.noreply.github.com>
2020-09-10 10:35:58 +0700

    update hien thi so nguyen to nho hon 100

--- Module2/src/_02_loop_in_java/exercises/HienThiCacSoNguyenToNhoHon100.java
+++
@@ -1,7 +0,0 @@
-package _02_loop_in_java.exercises;
-
-public class HienThiCacSoNguyenToNhoHon100 {
-    public static void main(String[] args) {
-
-    }
-}
--- Module2/src/_02_loop_in_java/exercises/HienThiSoNguyenToNhoHon100.java
+++ Module2/src/_02_loop_in_java/exercises/HienThiSoNguyenToNhoHon100.java
@@ -1,14 +1,8 @@
 package _02_loop_in_java.exercises;
 
-import java.util.Scanner;
-
-public class HienThi20SoNguyenToDauTien {
+public class HienThiSoNguyenToNhoHon100 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("please input the amount of primitive number to display");
-        int amount = sc.nextInt();
-        int num = 1;
-        for (int i=0; i<amount; num++) {
+        for (int num=1; num <100; num++) {
             boolean c = false;
             for (int b=2; b < num; b++) {
                 if (num % b == 0) {
@@ -18,7 +12,6 @@ public class HienThi20SoNguyenToDauTien {
             }
             if (!c) {
                 System.out.print(num +"\t");
-                i++;
             }
         }
     }
