09ccd81e65472688abdc0086f2f6512a22f8e3de
C0720G1-TranAnhKhoa <khoa.trananh2@gmail.com>
2020-09-21 09:11:42 +0700

    update refactoring and clean code

---
+++ Module2/src/Test/TestPrivateAtt.java
@@ -0,0 +1,8 @@
+package Test;
+
+public class TestPrivateAtt extends TestPrivateAttribute {
+    @Override
+    public String toString() {
+        return "TestPrivateAtt{} " + this.getNum();
+    }
+}
