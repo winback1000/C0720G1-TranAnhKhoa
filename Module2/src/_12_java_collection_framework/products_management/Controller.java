e7c5cd7dc9779ff3254923d975d88e04273076c1
C0720G1-TranAnhKhoa <khoa.trananh2@gmail.com>
2020-09-25 09:16:36 +0700

    update product management

---
+++ Module2/src/_12_java_collection_framework/products_management/Controller.java
@@ -0,0 +1,8 @@
+package _12_java_collection_framework.products_management;
+
+
+public class Controller {
+    public static void main(String[] args) {
+
+    }
+}
--- Module2/src/_12_java_collection_framework/products_management/ProductManager.java
+++ Module2/src/_12_java_collection_framework/products_management/ProductManager.java
@@ -10,61 +10,98 @@ public class ProductManager {
     static String tempManufacturer;
     static Float tempPrice;
     static String tempId;
-    static Integer tempIndex;
+    static int tempIndex;
+    static ArrayList<Product> filterList = new ArrayList<>();
 
     public static void mainMenu() {
         System.out.println("1. Add product \n2. Edit product\n3. Delete product\n4. Display list\n5. Search product\n6. Sort by");
-    }
-
-    public static void getInfo(){
-        System.out.print("Please input product's name\n");
-        scanner.nextLine();
-        tempName = scanner.nextLine();
-        System.out.println("please input the manufacturer");
-        tempManufacturer = scanner.nextLine();
-        System.out.println("Please input the price");
-        tempPrice = scanner.nextFloat();
-
-    }
-    public static void addProduct(String name, Float price, String manufacturer) {
-        productList.add(new Product(name,price,manufacturer));
-    }
-    public static void editProduct(String name, Float price, String manufacturer, Integer index){
-        productList.get(index).setName(name);
-        productList.get(index).setPrice(price);
-        productList.get(index).setManufacturer(manufacturer);
-    }
-    public static void searchProduct () {
-        System.out.println("1. By ID\n2. By Name\n3. By price\n4. By Manufacturer");
-        Byte selection = scanner.nextByte();
-        switch (selection){
+        int selection = scanner.nextInt();
+        switch (selection) {
             case 1:
-                System.out.println("please enter ID");
-                tempId = scanner.nextLine();
+                getInfo();
+                addProduct(tempName, tempPrice, tempManufacturer);
+                displayList();
+                mainMenu();
+                break;
+            case 2:
+                System.out.println("pls search the product you want to edit");
+                searchProductSubMenu();
+                getInfo();
+                editProduct(tempName, tempPrice,tempManufacturer,tempIndex);
+                displayList();
+                mainMenu();
+                break;
+            case 3:
+                System.out.println("pls select the product you want to delete");
+                searchProductSubMenu();
+                productList.remove(tempIndex);
+                displayList();
+                mainMenu();
+                break;
+            case 4:
+                displayList();
+                mainMenu();
+                break;
+            case 5:
+                searchProductSubMenu();
+
         }
-
-    }
-    public static void displayList() {
-        System.out.println(productList);
     }
 
-    public static void main(String[] args) {
+        public static void getInfo () {
+            System.out.print("Please input product's name\n");
+            scanner.nextLine();
+            tempName = scanner.nextLine();
+            System.out.println("please input the manufacturer");
+            tempManufacturer = scanner.nextLine();
+            System.out.println("Please input the price");
+            tempPrice = scanner.nextFloat();
 
-       mainMenu();
-       int selection = scanner.nextInt();
-       switch (selection){
-           case 1:
-               getInfo();
-               addProduct(tempName, tempPrice, tempManufacturer);
-               displayList();
-               mainMenu();
-               break;
-           case 2:
-               System.out.println("pls search the product you want to edit");
-               searchProduct();
+        }
+        public static void addProduct (String name, Float price, String manufacturer){
+            productList.add(new Product(name, price, manufacturer));
+        }
+        public static void editProduct (String name, Float price, String manufacturer, int index){
+            productList.get(index).setName(name);
+            productList.get(index).setPrice(price);
+            productList.get(index).setManufacturer(manufacturer);
+        }
+        public static void searchProductSubMenu() {
+            System.out.println("1. By ID\n2. By Name\n3. By price\n4. By Manufacturer");
+            byte selection = scanner.nextByte();
+            switch (selection) {
+                case 1:
+                    System.out.print("please enter ID\n");
+                    tempId = scanner.nextLine();
+                    searchById(tempId);
+                    break;
+                case 2:
+                    System.out.println("please enter product's name");
+                    tempName = scanner.nextLine();
+                    for (Product prd: productList) {
+                        if(prd.getName().equals(tempName)) {
+                            filterList.add(prd);
 
+                        }
+                    }
 
-       }
+            }
+        }
+            public static void searchById (String id){
+                for (int i = 0; i < productList.size(); i++) {
+                    if (productList.get(i).idProduct.equals(id)) {
+                        tempIndex = i;
+                    }
+                }
 
+            }
+
+        public static void displayList () {
+            for (Product prd: productList) {
+                System.out.println(prd);
+            }
+        }
     }
-}
+
+
+
